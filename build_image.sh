2f12bcd77f0205722eaef8cc2be9eb21a5d2b078
Vasiliy Solovey <miltador@yandex.ua>
2016-10-15 18:49:59 +0300

    Ask questions before building

--- build_image.sh
+++ build_image.sh
@@ -9,6 +9,12 @@
 
 rm -r /tmp/lede-imagebuilder-ar71xx-generic.Linux-x86_64
 
+# Ask for a root password
+read -s -p "Enter your router's root pasword: " CLEAR_PASS
+
+# Ask for a Wi-Fi password
+read -s -p "Enter your own Wi-Fi password: " WIFI_PASS
+
 # Download Image Builder
 wget -O /tmp/image-builder.tar.xz https://downloads.lede-project.org/snapshots/targets/ar71xx/generic/lede-imagebuilder-ar71xx-generic.Linux-x86_64.tar.xz
 
@@ -22,18 +28,12 @@ cd /tmp/lede-imagebuilder-ar71xx-generic.Linux-x86_64
 # Preparation
 sudo apt-get install build-essential libncurses5-dev zlib1g-dev gawk git ccache gettext libssl-dev xsltproc wget -y
 
-# Ask for a root password
-read -s -p "Enter your router's root pasword: " CLEAR_PASS
-
 # Generate root password hash
 RAND_STRING="$(openssl rand -hex 5)"
 HASH="$(openssl passwd -1 -salt $RAND_STRING $CLEAR_PASS)"
 
 # Write hash to file
 sed -i -- "s/root::0:0:99999:7:::/root:$(echo $HASH | sed -e 's/\\/\\\\/g; s/\//\\\//g; s/&/\\\&/g'):17082:0:99999:7:::/g" "files/etc/shadow"
-
-# Ask for a Wi-Fi password
-read -s -p "Enter your own Wi-Fi password: " WIFI_PASS
 
 # Write Wi-Fi password to file
 sed -i -- "s/OWN_WIFI_PASS_PLACEHOLDER/$WIFI_PASS/g" "files/etc/config/wireless"
