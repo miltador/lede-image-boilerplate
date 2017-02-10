e1a31330025a6c2b95288250b2e2ba0323866dd5
Vasiliy Solovey <miltador@yandex.ua>
2017-02-10 04:33:28 +0200

    Update to LEDE 17.01-rc2

--- build_image.sh
+++ build_image.sh
@@ -10,10 +10,10 @@ PACKAGES="luci-ssl-openssl luci-app-upnp luci-app-adblock luci-app-sqm sqm-scrip
 FILES="files/"
 
 # Script vars
-IMAGEBUILDER_URI="https://downloads.lede-project.org/releases/17.01.0-rc1/targets/ar71xx/generic/lede-imagebuilder-17.01.0-rc1-ar71xx-generic.Linux-x86_64.tar.xz"
+IMAGEBUILDER_URI="https://downloads.lede-project.org/releases/17.01.0-rc2/targets/ar71xx/generic/lede-imagebuilder-17.01.0-rc2-ar71xx-generic.Linux-x86_64.tar.xz"
 BUILD_DEPS="build-essential libncurses5-dev zlib1g-dev gawk git ccache gettext libssl-dev xsltproc wget"
 
-rm -r /tmp/lede-imagebuilder-17.0.1.0-rc1-ar71xx-generic.Linux-x86_64
+rm -r /tmp/lede-imagebuilder-17.0.1.0-rc2-ar71xx-generic.Linux-x86_64
 
 # Download Image Builder
 wget -O /tmp/image-builder.tar.xz $IMAGEBUILDER_URI
@@ -21,9 +21,9 @@ wget -O /tmp/image-builder.tar.xz $IMAGEBUILDER_URI
 # Extract the archive
 tar -xf /tmp/image-builder.tar.xz -C /tmp/
 
-cp -r $FILES /tmp/lede-imagebuilder-17.01.0-rc1-ar71xx-generic.Linux-x86_64
+cp -r $FILES /tmp/lede-imagebuilder-17.01.0-rc2-ar71xx-generic.Linux-x86_64
 
-cd /tmp/lede-imagebuilder-17.01.0-rc1-ar71xx-generic.Linux-x86_64
+cd /tmp/lede-imagebuilder-17.01.0-rc2-ar71xx-generic.Linux-x86_64
 
 # Preparation
 #sudo apt-get install $BUILD_DEPS -y
