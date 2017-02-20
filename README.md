f49010b0ff1f664407f9bec2e7b51fe8b6a1152f
Vasiliy Solovey <miltador@yandex.ua>
2017-02-20 23:38:44 +0200

    update script and configs, improve README.md

--- README.md
+++ README.md
@@ -1,2 +1,24 @@
 # lede-image-builder
-A set of my configs and an image builder for LEDE
+A set of my configs and an image build script for LEDE
+
+The main script is `build_image.sh`. It will download image builder and build it with the preset of packages and configs.
+
+## Build requirements
+
+```bash
+# Debian/Ubuntu
+sudo apt install -y build-essential libncurses5-dev zlib1g-dev gawk git ccache gettext libssl-dev xsltproc wget
+```
+
+## Usage
+
+```bash
+nano build_image.sh
+# edit script vars to suit your needs
+
+# then build image
+./build_image.sh
+
+# generated image will be in /tmp
+cd /tmp/lede-image-builder-*
+```
--- build_image.sh
+++ build_image.sh
@@ -4,14 +4,16 @@
 
 # Written by Vasiliy Solovey, 2016-2017
 
+# LEDE version
+LEDE_RELEASE="17.01.0"
+
 # Image Builder settings
 PROFILE="tl-wr842n-v1"
-PACKAGES="luci-ssl-openssl luci-app-upnp luci-app-adblock luci-app-sqm sqm-scripts dnscrypt-proxy"
+PACKAGES="luci-ssl-openssl luci-app-upnp luci-app-sqm sqm-scripts dnscrypt-proxy"
 FILES="files/"
 
 # Script vars
-IMAGEBUILDER_URI="https://downloads.lede-project.org/releases/17.01.0-rc2/targets/ar71xx/generic/lede-imagebuilder-17.01.0-rc2-ar71xx-generic.Linux-x86_64.tar.xz"
-BUILD_DEPS="build-essential libncurses5-dev zlib1g-dev gawk git ccache gettext libssl-dev xsltproc wget"
+IMAGEBUILDER_URI="https://downloads.lede-project.org/releases/$LEDE_RELEASE/targets/ar71xx/generic/lede-imagebuilder-$LEDE_RELEASE-ar71xx-generic.Linux-x86_64.tar.xz"
 
 rm -r /tmp/lede-imagebuilder-17.0.1.0-rc2-ar71xx-generic.Linux-x86_64
 
@@ -21,12 +23,9 @@ wget -O /tmp/image-builder.tar.xz $IMAGEBUILDER_URI
 # Extract the archive
 tar -xf /tmp/image-builder.tar.xz -C /tmp/
 
-cp -r $FILES /tmp/lede-imagebuilder-17.01.0-rc2-ar71xx-generic.Linux-x86_64
+cp -r $FILES /tmp/lede-imagebuilder-$LEDE_RELEASE-ar71xx-generic.Linux-x86_64
 
-cd /tmp/lede-imagebuilder-17.01.0-rc2-ar71xx-generic.Linux-x86_64
-
-# Preparation
-#sudo apt-get install $BUILD_DEPS -y
+cd /tmp/lede-imagebuilder-$LEDE_RELEASE-ar71xx-generic.Linux-x86_64
 
 # Make image
 make image PROFILE=$PROFILE PACKAGES="$PACKAGES" FILES=$FILES
